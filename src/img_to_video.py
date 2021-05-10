91c4d644605fe3642cc38e7e393d433859d81e33
blazer502 <ppoo1220@gmail.com>
2021-05-10 23:38:01 +0900

    Add guidelines to compile an image and audio

---
+++ src/img_to_video.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os
+import sys
+from moviepy.editor import *
+
+
+# Dependency
+IMG_DIR=""
+SOUND_DIR="sample/"
+OUT_DIR="result/"
+
+
+def main():
+    # Get an audio
+    # @TEST: sample audio
+
+
+    # Get an inserted image or video, which we made by analyging audio source
+    # @TEST: simple image first
+
+
+    # Configure the output video
+    # @HELP: How many moviepy provide options, e.g. FPS
+
+
+    # Merge the video and the audio source
+
+
+    # Compile the audio and the image to a video format
+    # @HELP: which format movepy is providing?
+
+
+    return 0
+
+
+
+
+# Starting Point of this code
+if __name__ == "__main__":
+    # (option) Gets Arguments from the terminal
+    #          it may need when we connect this python backend to flutter frontend
+
+    main()
